9bd287f0f9d31888c03da30861db0d646dce896d
serg-e <smosesov@gmail.com>
2020-05-02 19:02:58 +0100

    buil

---
+++ web/backgammon_react_app/build/precache-manifest.5e5ab86c3faa6fb7e715ecb24ac38e1e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c6e4f7ecbf93571d20ebcb78c6e3296",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0edacca45434075c1d5c",
+    "url": "/static/css/main.83249b26.chunk.css"
+  },
+  {
+    "revision": "11299340d00fc2c0c499",
+    "url": "/static/js/2.34c41b85.chunk.js"
+  },
+  {
+    "revision": "f48e01ec5cb1975aa69267de1e6cff37",
+    "url": "/static/js/2.34c41b85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0edacca45434075c1d5c",
+    "url": "/static/js/main.bbd8b358.chunk.js"
+  },
+  {
+    "revision": "22f764883fe1ec7b395d",
+    "url": "/static/js/runtime-main.eb282790.js"
+  },
+  {
+    "revision": "56ef43f5325ccf1592a19a2cf290c03d",
+    "url": "/static/media/bar2.56ef43f5.jpeg"
+  },
+  {
+    "revision": "1968a691d9d8ddcd9135d71eb0580ba0",
+    "url": "/static/media/board.1968a691.jpg"
+  }
+]);
